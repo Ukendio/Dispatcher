617a7901a4f70ae720e3856fff5385a5c13e2a4d
Marcus <Ukendio@gmail.com>
2021-07-18 00:46:16 +0200

    remove ci

--- tests/src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { ServerScriptService } from "@rbxts/services";
-import TestEz from "@rbxts/testez";
-
-const results = TestEz.TestBootstrap.run([ServerScriptService.tests]);
-if (results.errors.size() > 0 || results.failureCount > 0) {
-	xpcall();
-}
--- tests/src/services.d.ts
+++
@@ -1,6 +0,0 @@
-interface ServerScriptService extends Instance {
-	tests: Folder;
-}
-interface ReplicatedStorage extends Instance {
-	include: { node_modules: { testez: { src: ModuleScript } } };
-}
